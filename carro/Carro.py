167f9cb670e56ae93aa8b53986261acd0680fac6
bayron74 <88299465+bayron74@users.noreply.github.com>
2021-10-30 10:01:02 -1200

    primer commit

---
+++ carro/Carro.py
@@ -0,0 +1,13 @@
+class Carro:
+
+
+    def encender (self):
+        return "Carro encendido"
+
+    
+    def frenar (self):
+        return "frenando carro"
+    
+
+    def acelerar (self):
+        return "Carro en marcha"
---
+++ carro/clasePrincipal.py
@@ -0,0 +1,5 @@
+from Carro import Carro
+
+x = Carro()
+
+print (x.encender())
