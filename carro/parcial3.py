67a8caed5767679b3b6d1508068e251f0e6748da
bayron74 <88299465+bayron74@users.noreply.github.com>
2021-11-13 11:27:42 -1200

    tercer commit

---
+++ carro/parcial3.py
@@ -0,0 +1,11 @@
+class logica:
+    
+    def mayordedatos(self):
+        x = int(input ("Ingrese un numero"))
+        print(x)
+
+        y = int(input("Ingrese en siguiente nuemero"))
+        print(y)
+        if x > y:
+            return("Numero mayor")
+    
---
+++ carro/principal.py
@@ -0,0 +1,8 @@
+from mayoresdedato import logica
+
+r = logica()
+
+print(r.mayordedato())
+
+
+#hize lo que pude :(
